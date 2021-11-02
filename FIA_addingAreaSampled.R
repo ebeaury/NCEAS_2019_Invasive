4fea1fc77150b731d43cd87913cd72f6f017f1c0
Lais Petri <petri@aurora.nceas.ucsb.edu>
2021-11-02 14:54:15 -0700

    Retrieve all the plots (including the ones with resampling) and generate a file with the resampled ones so I can change their plot codes.

--- FIA_addingAreaSampled.R
+++ FIA_addingAreaSampled.R
@@ -2,8 +2,10 @@
 ## Goal1: add the total area sampled ##
 ## Goal2: remove undesired columns (DESIGNCD and traits columns) ##
 
-FIAdata<- read.csv('/home/shares/neon-inv/data_paper/data_by_dataset/archived/FIA Veg data Latest Traits 8-4-20.csv', 
-                    header = T, stringsAsFactors = F)
+# FIAdata<- read.csv('/home/shares/neon-inv/data_paper/data_by_dataset/archived/FIA Veg data Latest Traits 8-4-20.csv', 
+                    # header = T, stringsAsFactors = F) #this file just have the last year each plot was surveyed
+FIAdata<- read.csv('/home/shares/neon-inv/data_paper/data_by_dataset/archived/FIA Veg data Traits 8-4-20.csv', 
+                   header = T, stringsAsFactors = F)
 FIAdata <- select(FIAdata, -X)
 
 FIAtotalArea <- read.csv('/home/shares/neon-inv/data_paper/code_by_dataset/extra_csv_FIA/FIA_PlotArea_8-4-20.csv',
@@ -12,7 +14,7 @@ FIAtotalArea <- select(FIAtotalArea, -X)
 
 FIAdatafinal <- FIAdata %>%
   left_join(FIAtotalArea) %>%
-  select(-DESIGNCD, -STATECD, -DESIGNCD, -c(18:28))
+  select(-DESIGNCD, -DESIGNCD, -c(18:28))
 
 ##checking if all plots have associated SampledArea info##
 table(FIAdatafinal$SampledArea)
@@ -25,7 +27,19 @@ FIAdatafinal %>%
 #plot 85 for a few entries has no cover information associated. Although the year of data collection is 
 # the same as the other rows for this plot, they have different PLT_CN
 
+# finding the number of plots that were resampled
+Resample <- aggregate(INVYR ~ PLOT+STATECD+LAT+LON, data=unique(FIAdatafinal[c("PLOT","INVYR", "STATECD", "LAT", "LON")]), FUN=length)
+Resample <- Resample [order(Resample$INVYR, decreasing = TRUE), ]
+## creates a file with unique plotIDs for FIA data
+Resample %>% 
+  mutate(NewPlot = paste0("FIA_", 1:2715)) %>%
+  select(-INVYR) %>% 
+  write.csv("/home/shares/neon-inv/data_paper/code_by_dataset/extra_csv_FIA/NewPlotIDFIA.csv", row.names = F)
+
+Twice <- subset (Resample, INVYR == 2) 
+write.csv (Twice, "Resampled plots.csv")
 
 ##exporting file##
-write.csv(FIAdatafinal, "/home/shares/neon-inv/data_paper/data_by_dataset/FIA_DataPaper_07292021.csv",
+write.csv(FIAdatafinal, "/home/shares/neon-inv/data_paper/data_by_dataset/FIA_DataPaper_10282021.csv",
           row.names = FALSE)
+
